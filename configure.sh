bf4e7682af699048017764893590a558208f2880
Daniel Balcells <dbalcells@gmail.com>
2016-05-12 18:20:52 +0200

    Fixed path issue

--- configure.sh
+++ configure.sh
@@ -1,12 +1,11 @@
 #! /bin/bash
-# Make aliases for dotfiles
-# This should be run from the dotfiles repo root directory
+# Make aliases for .dotfiles
 
 # Make a copy of the sourcing file
 # Edit this individually for each host
-cp ./sources ~/.bash_profile
+cp ~/.dotfiles/sources ~/.bash_profile
 ln -s -i ~/.bash_profile ~/.bash_aliases
 
 # Make symlinks for non-bash configuration files
-ln -s -i ./vimrc ~/.vimrc
-ln -s -i ./screenrc ~/.screenrc
+ln -s -i ~/.dotfiles/vimrc ~/.vimrc
+ln -s -i ~/.dotfiles/screenrc ~/.screenrc
