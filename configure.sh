57d04e5dd80fc49954030fcd51707e8393b75724
Daniel Balcells <dbalcells@gmail.com>
2016-05-12 18:50:02 +0200

    Riduculous infinite loop

--- configure.sh
+++ configure.sh
@@ -6,7 +6,7 @@ repo_path=~/.dotfiles
 # Make a copy of the sourcing file
 # Edit this individually for each host
 cp $repo_path/sources ~/.bash_profile
-ln -s -i ~/.bash_profile ~/.bash_aliases
+ln -s -i ~/.bash_profile ~/.bashrc
 
 # Make symlinks for non-bash configuration files
 ln -s -i $repo_path/vimrc ~/.vimrc
