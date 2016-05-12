332938bb8ee604443d5818fac61f6a2bb98dfce3
Daniel Balcells <dbalcells@gmail.com>
2016-05-12 18:29:33 +0200

    Added git config

--- configure.sh
+++ configure.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 # Make aliases for .dotfiles
+repo_path=~/.dotfiles
+
 
 # Make a copy of the sourcing file
 # Edit this individually for each host
-cp ~/.dotfiles/sources ~/.bash_profile
+cp $repo_path/sources ~/.bash_profile
 ln -s -i ~/.bash_profile ~/.bash_aliases
 
 # Make symlinks for non-bash configuration files
-ln -s -i ~/.dotfiles/vimrc ~/.vimrc
-ln -s -i ~/.dotfiles/screenrc ~/.screenrc
+ln -s -i $repo_path/vimrc ~/.vimrc
+ln -s -i $repo_path/screenrc ~/.screenrc
+ln -s -i $repo_path/gitconfig ~/.gitconfig
